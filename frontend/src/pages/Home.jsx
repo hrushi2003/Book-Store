f287cf4d40b519ae035c69403987acd037a2b233
Hrushikesh Vandanapu <94824558+hrushi2003@users.noreply.github.com>
2024-02-14 16:18:33 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
 import axios from "axios"
-import Spinner from '../components/spinner';
+import Spinner from '../components/Spinner';
 import { Link } from "react-router-dom";
 import {AiOutlineEdit} from "react-icons/ai";
 import {BsInfoCircle} from "react-icons/bs";
