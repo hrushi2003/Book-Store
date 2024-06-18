03873d4905f1547d77b0bf272a25223d52bb6412
Hrushikesh Vandanapu <94824558+hrushi2003@users.noreply.github.com>
2024-06-18 10:22:50 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ export const PORT = 3000;
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({path:'sample.env'});
-const URI = process.env.MONGO_URI;
+const URI = "mongodb+srv://hrushikesh2003:hrushi0406@cluster0.vjepk9n.mongodb.net/books-collection?retryWrites=true&w=majority";
 export const connect = async () => {
     try{
         await mongoose.connect(URI,{useNewUrlParser:true});
